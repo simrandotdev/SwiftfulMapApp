e409b725fd15ade201453882600827e240954b9e
Simran Preet Singh <simran5590@gmail.com>
2022-02-27 17:24:47 -0800

    Added Location to serve the locations

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Simran Preet Narang on 2022-02-27.
+//
+
+import Foundation
+import CoreLocation
+
+struct Location {
+    var name: String
+    var cityName: String
+    var coordinates: CLLocationCoordinate2D
+    var description: String
+    var imageNames: [String]
+    var link: String
+}
